1e7b7926dbbd53e095998cd45d61f6f59eaff83f
Michael O. Church <michael.o.church@gmail.com>
2014-04-18 21:03:12 -0400

    adding Nim game

---
+++ src/wyvern/nim.clj
@@ -0,0 +1,41 @@
+(ns wyvern.nim
+  (:use wyvern.types
+        wyvern.utils))
+
+(defrecord State [active stones])
+
+(def ^:const stones-per-turn-limit 3)
+
+(defn nim-move-1 [game-state player action]
+  (if (= action :no-op)
+    game-state
+    (update game-state
+            :active #(rem (inc %) 2)
+            :stones #(- % action))))
+
+(defn nim-move [game-state player-actions]
+  (reduce (fn [state [player action]]
+            (nim-move-1 state player action))
+          game-state
+          player-actions))
+
+(defrecord NimGame [state]
+  Game
+  ; two players named 0 and 1
+  (all-players [this] [0 1])
+  ; perfect information -- view is full game state.
+  (view [this _] state)
+  ; actions are :no-op and {1, 2, 3} (number of stones taken).
+  (legal-actions [this player-id]
+    (if (= player-id (:active state))
+      (range 1 (min (inc stones-per-turn-limit) (:stones state)))
+      :no-op))
+  ; a Nim move is to take stones off the board.
+  (move [this player-actions]
+    (nim-move state player-actions))
+  ; game ends when no stones are left. 
+  (terminal? [this]
+    (= (:stones state) 0))
+  ; active player at game end is loser (score = 0.0) and other wins (1.0).
+  (score [this player-id]
+    (if (= (:active state) player-id) 0.0 1.0)))
