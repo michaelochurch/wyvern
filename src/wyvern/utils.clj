d77db8eafd3c78384e57d5fa5b87072c292cf9e3
Michael O. Church <michael.o.church@gmail.com>
2014-04-24 21:32:57 -0400

    add image-map to utils.clj

--- src/wyvern/utils.clj
+++ src/wyvern/utils.clj
@@ -6,6 +6,12 @@
       (throw (Exception. (format "get!: map %s missing key %s" m k)))
       ret)))
 
+(defn image-map [f coll]
+  (reduce (fn [m x]
+            (assoc m x (f x)))
+          {}
+          coll))
+
 (defn map-values [f m]
   (let [init-val (if (record? m) m (empty m))]
     (reduce (fn [acc [k v]]
