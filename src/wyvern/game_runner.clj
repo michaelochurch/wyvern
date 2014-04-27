0766b5734efa2594677160a026f1ed1444812d91
Michael O. Church <michael.o.church@gmail.com>
2014-04-27 13:52:20 -0400

    first pass at game_runner

--- src/wyvern/game_runner.clj
+++ src/wyvern/game_runner.clj
@@ -24,14 +24,46 @@
 
 (defn game-instance [game & {:keys [n-players config player-ids player-fns on-view]}]
   (let [n-players (or n-players (first (:n-players game)))
-        player-ids (or player-ids (default-player-ids n-players))]
-    {:game game
-     :state ((:init game) config)
+        player-ids (or player-ids (default-player-ids n-players))
+        init-state ((:init game) config)]
+    {:game       game
+     :state      init-state
      :player-ids player-ids
      :player-fns (or player-fns (default-player-fns game player-ids))
+     :history    [[:init init-state]]
      :on-view    (or on-view    (default-on-view player-ids))}))
 
-(defn run-game-instance [game-instance]
-)
+(defn get-view [game-instance player-id]
+  ((get-in game-instance [:game :view]) (:state game-instance) player-id))
 
+(defn run-player-actions [game-instance]
+  (into {}
+        (map (fn [[player-id f]]
+               [player-id (f (get-view game-instance player-id))])
+             (:player-fns game-instance))))
+
+(defn check-player-actions [game-instance actions]
+  (let [legal? (-> game-instance :game :legal)
+        check  #(legal? (:state game-instance) %1 %2)]
+    (every? (fn [[player-id action]]
+              (check player-id action))
+            actions)))
+
+(defn run-game-instance-one-step [game-instance]
+  (let [actions (run-player-actions game-instance)
+        new-state ((get-in game-instance [:game :move]) 
+                   (:state game-instance) actions)]
+    (when-not (check-player-actions game-instance actions)
+      (throw (Exception. (format "run-game-instance-one-step: received illegal actions"))))
+    (update game-instance :state (fn [_] new-state)
+            :history #(conj % [actions new-state]))))
+
+(defn terminal-check [game-instance]
+  ((get-in game-instance [:game :terminal?]) (get game-instance :state)))
+   
+(defn run-game-instance [game-instance]
+  (loop [gi game-instance]
+    (if (terminal-check gi)
+      gi
+      (recur (run-game-instance-one-step gi)))))
 
